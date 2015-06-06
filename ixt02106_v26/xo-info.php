2109077af941e7561ac9c9d40a67460b80050d9b
mambax7 <mambax7@gmail.com>
2015-06-06 07:59:57 -0400

    Original files

---
+++ ixt02106_v26/xo-info.php
@@ -0,0 +1,37 @@
+<?php
+/*
+* Qualitative Themes for XOOPS
+*
+* @license       http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+* @project       IXThemes Project, http://ixthemes.org
+* @theme_name    ixt02106
+* @theme_demo    http://ixthemes.sourceforge.net/demo/x/index.php?xoops_theme_select=ixt02106
+* @theme_author  Alexander Galochkin (algalochkin at users.sourceforge.net)
+* @version       $Id: xo-info.php 2653 2010-03-28 13:41:59Z algalochkin $
+*
+* Thanks you, that you have left this header untouched!!!
+*/
+return array(
+ "languages"   => array("main"),
+ "copyright"   => "&copy; IXThemes Project <http://ixthemes.org>",
+ "name"        => "ixt02106",
+ "description" => "XOOPS 2.0.1x, 2.3.x, 2.4.x and 2.5.x, fixed width 1000px, 3 columns, slides, ext.footer, ext.header, multiskin",
+ "version"     => "2.6",
+ "format"      => "XOOPS",
+ "author"      => "IXThemes Project, support@ixthemes.org",
+ "demo"        => "http://ixthemes.sourceforge.net/demo/x/index.php?xoops_theme_select=ixt02106",
+ "demo1"       => "http://xoops233demo.ixthemes.org/index.php?xoops_theme_select=ixt02106",
+ "demo2"       => "http://xoops240demo.ixthemes.org/index.php?xoops_theme_select=ixt02106",
+ "demo3"       => "http://xoops241demo.ixthemes.org/index.php?xoops_theme_select=ixt02106",
+ "demo4"       => "http://xoops242demo.ixthemes.org/index.php?xoops_theme_select=ixt02106",
+ "demo5"       => "http://xoops243demo.ixthemes.org/index.php?xoops_theme_select=ixt02106",
+ "demo6"       => "http://xoops244demo.ixthemes.org/index.php?xoops_theme_select=ixt02106",
+ "demo7"       => "http://xoops250demo.ixthemes.org/index.php?xoops_theme_select=ixt02106",
+ "url"         => "http://ixthemes.org",
+ "download"    => "http://ixthemes.com",
+ "W3C"         => "XHTML 1.0 Transitional - CSS 1/2/3",
+ "licence"     => "GPL",
+ "thumbnail"   => "shot.gif",
+ "screenshot"  => "screenshot.jpg",
+);
+?>
